88d0723fe429a69bf9f7160ffaf238d62b3018dd
leehunbok <rmsidrmfo1537@gmail.com>
2024-11-11 13:09:41 +0900

    프로젝트 생성 & application.properties 설정

---
+++ web-cafe/src/main/java/com/web/cafe/WebCafeApplication.java
@@ -0,0 +1,13 @@
+package com.web.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCafeApplication.class, args);
+	}
+
+}
